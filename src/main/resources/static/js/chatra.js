e375d0fdf6050b035629c3de193d198243d74876
ADMIN <serezliev@gmail.com>
2024-05-09 05:18:28 +0300

    Support services

---
+++ src/main/resources/static/js/chatra.js
@@ -0,0 +1,19 @@
+
+    window.ChatraSetup = {
+    colors: {
+    buttonText: '#ffffff', /* chat button text color */
+    buttonBg: '#925631'    /* chat button background color */
+}
+};
+
+
+(function (d, w, c) {
+    w.ChatraID = 'JhRHmiuev9mm7TEF7';
+    var s = d.createElement('script');
+    w[c] = w[c] || function () {
+        (w[c].q = w[c].q || []).push(arguments);
+    };
+    s.async = true;
+    s.src = 'https://call.chatra.io/chatra.js';
+    if (d.head) d.head.appendChild(s);
+})(document, window, 'Chatra');
